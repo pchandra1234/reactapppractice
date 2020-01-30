830538df0651c03581135c33ae3bbbf3fcc64d16
pchandra1234 <pradeepchittipch@example.com>
2020-01-30 01:34:20 -0600

    react: Added code related to list split slice map

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,26 +1,15 @@
 import React from 'react';
-import './Person.css'
-const person = (props) => {
-return (
-    <div className="Person">
-        <p onClick={props.click}>I'm a {props.name}! and I am {props.age} years old</p>
-<p>{props.children}</p>
-<input type="text"  onChange={props.changed} 
-value = {props.name}
-/>
-    </div>
-)
 
-//<p>I'm a {props.name}! and I am {props.age} years old</p>
+import './Person.css';
+
+const person = ( props ) => {
+    return (
+        <div className="Person">
+            <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
+            <p>{props.children}</p>
+            <input type="text" onChange={props.changed} value={props.name} />
+        </div>
+    )
 };
 
-/*
-const test = () =>{
-    let a = Math.floor(Math.random() * 30);
-    return a;
-}
-*/
-
 export default person;
-
-// benfits of creating the new components
