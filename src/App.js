1faca7fbaafcbfda917443e3e441b98ea0f43dee
pchandra1234 <pradeepchittipch@example.com>
2020-01-31 02:53:07 -0600

    added styling changes

--- src/App.js
+++ src/App.js
@@ -46,11 +46,16 @@ class App extends Component {
 
   render () {
     const style = {
-      backgroundColor: 'white',
+      backgroundColor: 'green',
+      color: 'white',
       font: 'inherit',
       border: '1px solid blue',
       padding: '8px',
-      cursor: 'pointer'
+      cursor: 'pointer',
+      ':hover': {
+        backgroundColor: 'salmon',
+        color: 'black'
+      }
     };
 
     let persons = null;
@@ -68,20 +73,34 @@ class App extends Component {
           })}
         </div>
       );
+
+      style.backgroundColor = 'red';
     }
 
+    let classes = [];
+    if(this.state.persons.length <=2) {
+      classes.push('red');
+    }
+    if(this.state.persons.length <=1) {
+      classes.push('bold');
+    }
+    
+
     return (
+     
       <div className="App">
         <h1>Hi, I'm a React App</h1>
-        <p>This is really working!</p>
+        <p className={classes.join(' ')}>This is really working!</p>
         <button
           style={style}
           onClick={this.togglePersonsHandler}>Toggle Persons</button>
         {persons}
       </div>
+     
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
   }
 }
 
 export default App;
+//export default Radium(App);
